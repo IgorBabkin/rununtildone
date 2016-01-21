c372dfd22d4132585992b44010c42cde8a9a343f
IgorBabkin <igba14@gmail.com>
2016-01-21 22:17:37 +0200

    add tests

--- lib/job.ts
+++ lib/job.ts
@@ -4,11 +4,11 @@ import { EventEmitter2 } from 'eventemitter2';
 import IGenerator from "./IGenerator";
 
 class Job {
-    public isRunning: boolean;
-    private timeoutId: number;
-    private eventEmitter: EventEmitter2;
+    public isRunning:boolean;
+    private timeoutId:number;
+    private eventEmitter:EventEmitter2;
 
-    constructor(private executeFn: Function, private delayGenerator: IGenerator) {
+    constructor(private executeFn:Function, private delayGenerator:IGenerator) {
         this.eventEmitter = new EventEmitter2();
     }
 
@@ -16,51 +16,57 @@ class Job {
         this.eventEmitter.on(event, listener);
     }
 
-    public once(event: string, listener: Function): void {
+    public once(event:string, listener:Function):void {
         this.eventEmitter.once(event, listener);
     }
 
-    public start(): void {
+    public start():void {
         this.emit('start');
         this.isRunning = true;
         this.run();
     }
 
-    public stop(): void {
+    public stop():void {
         clearTimeout(this.timeoutId);
         this.reset();
         this.emit('stop');
     }
 
-    private emit(event: string): void {
-        this.eventEmitter.emit(event);
+    private emit(event:string, data?):void {
+        this.eventEmitter.emit(event, data);
     }
 
-    private reset(): void {
+    private reset():void {
         this.delayGenerator.reset();
         this.isRunning = false;
     }
 
-    private done(): void {
+    private done(result?):void {
         this.reset();
-        this.emit('done');
+        this.emit('done', result);
     }
 
-    private run(): void {
-        this.execute().then(this.done, this.retry);
+    private run():void {
+        this.execute().then(
+            this.done.bind(this),
+            this.retry.bind(this)
+        );
     }
 
-    private retry(): void {
+    private retry():void {
         this.emit('retry');
         let delay = this.delayGenerator.next();
         this.timeoutId = this.runDefer(delay);
     }
 
-    private runDefer(delay: number): number {
-        return setTimeout(this.run, delay);
+    private runDefer(delay:number):number {
+        return setTimeout(
+            this.run.bind(this),
+            delay
+        );
     }
 
-    private execute(): Promise<any> {
+    private execute():Promise<any> {
         this.emit('execute');
         return this.executeFn();
     }
--- test/job_test.ts
+++ test/job_test.ts
@@ -1,26 +1,51 @@
-"use strict";
-
-import DefaultDelayGenerator from "../lib/delayGenerator";
-import { assert } from 'chai';
 import Job from "../lib/job";
+import DefaultDelayGenerator from "../lib/delayGenerator";
+import { expect } from 'chai';
 
-/// <reference path="../typings/mocha/mocha.d.ts" />
-/// <reference path="../typings/assertion-error/assertion-error.d.ts" />
-/// <reference path="../typings/chai/chai.d.ts" />
-
-
-describe('Job', function() {
-    describe('on', function () {
-        let job = new Job(
-            () => {
-                return Promise.resolve();
-            },
-            new DefaultDelayGenerator(5000)
-        );
-
-        it('should return -1 when the value is not present', function () {
-            assert.equal(-1, [1,2,3].indexOf(5));
-            assert.equal(-1, [1,2,3].indexOf(0));
+describe('Job', () => {
+    it('Scenario 1', (done) => {
+        let fn = () => new Promise((resolve) => {
+            setTimeout(resolve.bind(null, 'yo'), 500);
         });
+
+        let job = new Job(fn, new DefaultDelayGenerator(500));
+        let doJob = (job) => {
+            return new Promise((resolve, reject) => {
+                job.once('stop', reject);
+                job.once('done', resolve);
+                job.start();
+            });
+        };
+
+        let testResult = (msg) => {
+            expect(msg).to.equal('yo');
+            done();
+        };
+
+        doJob(job).then(testResult);
+    });
+
+    it('Scenario 2', (done) => {
+        let createFn = (count:number = 0) => () => {
+            return (count++ < 2) ? Promise.reject(count) : Promise.resolve(count);
+        };
+
+        let createDelayGenerator = (delay:number) => new DefaultDelayGenerator(delay);
+
+        let job = new Job(createFn(), createDelayGenerator(200));
+        let doJob = (job) => {
+            return new Promise((resolve, reject) => {
+                job.once('stop', reject);
+                job.once('done', resolve);
+                job.start();
+            });
+        };
+
+        let testResult = (count) => {
+            expect(count).to.equal(3);
+            done();
+        };
+
+        doJob(job).then(testResult);
     });
 });
